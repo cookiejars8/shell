75a134de529ea4669bb6d8cf1f10a82743b40bff
Cookie Jars <cookiejars8@users.noreply.github.com>
2025-11-09 12:49:28 -0500

    correcting question 7

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -47,7 +47,8 @@ cp -vi ./data/raw/*user*.log ./data/processed/user_logs/
 cp -vi ./data/raw/*event*.log ./data/processed/event_logs/
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
-rm -vi ./data/raw/*ipaddr* ./data/processed/user_logs/*ipaddr*
+rm -vi ./data/raw/*ipaddr*
+rm -vi ./data/processed/user_logs/*ipaddr*
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 find ./data/processed -type f > ./data/inventory.txt
